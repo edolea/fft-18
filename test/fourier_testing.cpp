c993b1cb03f88f9529bbe151e3426fac4f6890da
edolea <163143896+edolea@users.noreply.github.com>
2025-05-30 09:02:09 +0200

    gtest added

---
+++ test/fourier_testing.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Edoardo Leali on 24/05/25.
+//
+#include <gtest/gtest.h>
+#include "abstract_transform.hpp"
+
+TEST(SampleTest, Addition) {
+    EXPECT_EQ(2 + 2, 4);
+}
+
+TEST(FirstTest, Addition){
+    EXPECT_EQ(2, 2);
+}
