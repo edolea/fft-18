cf0f9cc7d9ff402397828555b32e7068b4bfa4d7
edolea <163143896+edolea@users.noreply.github.com>
2025-05-30 11:35:45 +0200

    matrix concept

--- include/abstract_transform.hpp
+++ include/abstract_transform.hpp
@@ -8,8 +8,8 @@
 #include <concepts>
 
 template <typename T = std::vector<std::complex<double>> >
-concept ComplexVectorPre = std::is_same_v<T, std::vector<std::complex<float>>> ||
-                        std::is_same_v<T, std::vector<std::complex<double>>>;
+concept ComplexVectorPre = std::is_same_v<T, std::vector<std::complex<float>> > ||
+                           std::is_same_v<T, std::vector<std::complex<double>> >;
 
 constexpr bool isPowerOfTwo(size_t n){
     return n > 0 && (n & (n-1)) == 0; // very efficient bc single bitwise operation
@@ -21,17 +21,32 @@ concept ComplexVector = ComplexVectorPre<T> && requires(const T& vec){
                                                                   // but left to double check at compile time if isPowerOfTwo is callable
 };
 
+template <typename T>
+concept ComplexMatrixPre = std::is_same_v<T, std::vector<std::vector<std::complex<float>>> > ||
+                           std::is_same_v<T, std::vector< std::vector<std::complex<double>>> >;
+
+template <typename T>
+concept ComplexMatrix = ComplexMatrixPre<T> && requires(const T& mat) {
+    { mat.size() } -> std::convertible_to<size_t>;
+    { mat[0].size() } -> std::convertible_to<size_t>;
+    requires !mat.empty() && !mat[0].empty();
+    { isPowerOfTwo(mat.size()) } -> std::convertible_to<bool>;
+    { isPowerOfTwo(mat[0].size()) } -> std::convertible_to<bool>;
+    requires mat.size() == mat[0].size(); // Ensure matrix is square
+};
+
 using complexDouble = std::complex<double>;
 using doubleVector = std::vector<complexDouble>;
 using doubleMatrix = std::vector<doubleVector>;
+
 using complexFloat = std::complex<float>;
 using floatVector = std::vector<complexFloat>;
+using floatMatrix = std::vector<floatVector>;
 
 
 // template specialization needed to check input
 template <ComplexVector T>
-class BaseTransform
-{
+class BaseTransform {
 protected:
     // TODO: remove default constructure and put input constant !!!
     const T input;
